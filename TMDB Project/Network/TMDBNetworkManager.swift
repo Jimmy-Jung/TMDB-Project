a58ea678026b3ce4538ddbb94c18018a0e996c84
Jimmy <joony300@gmail.com>
2023-08-20 14:42:54 +0900

    [Fix] 안쓰는 메서드 정리

--- TMDB Project/Network/TMDBNetworkManager.swift
+++ TMDB Project/Network/TMDBNetworkManager.swift
@@ -12,60 +12,11 @@ final class TMDBNetworkManager {
     static let shared = TMDBNetworkManager()
     private init() {}
     
-    func requestRecommendation(movieID: Int, completionHandler: @escaping ((_ data: Recommendations) -> Void)) {
-        let url =
-        "https://api.themoviedb.org/3/movie/\(movieID)/recommendations"
-        let headers: HTTPHeaders = [
-            "accept": "application/json",
-            "Authorization": APIKEY.TMDB_Acess_Token
-        ]
-        let parameters: Parameters = ["language": "ko"]
-        AF.request(url, method: .get, parameters: parameters, headers: headers).validate(statusCode: 200...500).responseDecodable(of: Recommendations.self) { response in
-            switch response.result {
-            case .success(let value):
-                completionHandler(value)
-            case .failure(let error):
-                print(error)
-            }
-        }
-    }
-    
-    func requestTrending(period: Period, completionHandler: @escaping (([MovieInfo]) -> Void)) {
-        let url = TMDB_API.Trendings.url(period: period)
-        let headers: HTTPHeaders = [
-            "accept": "application/json",
-            "Authorization": APIKEY.TMDB_Acess_Token
-        ]
-        let parameters: Parameters = ["language": "ko"]
-        AF.request(url, method: .get, parameters: parameters, headers: headers).validate().responseDecodable(of: Trendings.self) { response in
-            switch response.result {
-            case .success(let value):
-                guard let movieInfoList = value.results else {return}
-                completionHandler(movieInfoList)
-            case .failure(let error):
-                print(error)
-            }
-        }
-    }
-    
-    func requestCredits(movieID: Int, completionHandler: @escaping ((Credits) -> Void)) {
-        let url = TMDB_API.Credits.url(movieID: movieID)
-        let headers: HTTPHeaders = [
-            "accept": "application/json",
-            "Authorization": APIKEY.TMDB_Acess_Token
-        ]
-        let parameters: Parameters = ["language": "ko"]
-        AF.request(url, method: .get, parameters: parameters, headers: headers).validate().responseDecodable(of: Credits.self) { response in
-            switch response.result {
-            case .success(let value):
-                completionHandler(value)
-            case .failure(let error):
-                print(error)
-            }
-        }
-    }
-    
-    func requestTMDB<T: Decodable & TMDBResultType>(requestOption: TMDBRequestOption, metaType: T.Type, completionHandler: @escaping ((T) -> Void)) {
+    func requestTMDB<T: Decodable & TMDBResultType>(
+        requestOption: TMDBRequestOption,
+        metaType: T.Type,
+        completionHandler: @escaping ((T) -> Void)
+    ) {
         let url = requestOption.getURL
         let headers: HTTPHeaders = [
             "accept": "application/json",
